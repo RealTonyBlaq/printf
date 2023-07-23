24741e4b067a7af8926031846e07b74129248639
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-07-23 13:40:46 +0100

    The _printf function for %s, %c and % format specifiers

---
+++ printf.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+int _printf(const char *format, ...);
+int _strlen(char *str);
+
+/**
+ * _printf - My own printf function
+ * @format: Contains the format specifiers
+ *
+ * Return: The _printf count, 0 (success)
+ */
+
+int _printf(const char *format, ...)
+{
+	int printer = 0;
+	char *s, c;
+	va_list var;
+
+	va_start(var, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					c = va_arg(var, int);
+					printer += write(1, &c, 1);
+					break;
+				case 's':
+					s = va_arg(var, char *);
+					if (s != NULL)
+					{
+						printer += write(1, s, _strlen(s));
+						break;
+					}
+					printer += write(1, "(nil)", sizeof("(nil)") - 1);
+					break;
+				default:
+					printer += write(1, format, 1);
+					break;
+			}
+		}
+		else
+		{
+			printer += write(1, format, 1);
+		}
+		format++;
+	}
+	va_end(var);
+	return (printer);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: The string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
